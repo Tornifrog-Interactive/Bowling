49fc8ffc1872c3a8ed1566afef7847b6b7339be9
Ilan <ilan1il1000@gmail.com>
2023-04-25 00:16:13 +0300

    fix

--- Assets/Scripts/ArrowController.cs
+++ Assets/Scripts/ArrowController.cs
@@ -8,14 +8,16 @@ public class ArrowController : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            if(transform.rotation.eulerAngles.y >= 60){
+            if (transform.rotation.eulerAngles.y >= 60)
+            {
                 transform.Rotate(Vector3.down, Time.deltaTime * 30f);
             }
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            if(transform.rotation.eulerAngles.y <= 120){
+            if (transform.rotation.eulerAngles.y <= 120)
+            {
                 transform.Rotate(Vector3.up, Time.deltaTime * 30f);
             }
         }
@@ -24,12 +26,19 @@ public class ArrowController : MonoBehaviour
         {
             if (transform.localScale.x < 0.7f)
             {
-                transform.localScale = new Vector3(transform.localScale.x + (1 * Time.deltaTime), transform.localScale.y,
-                    transform.localScale.z);
+                transform.localScale = new Vector3(
+                    transform.localScale.x + (1 * Time.deltaTime),
+                    transform.localScale.y,
+                    transform.localScale.z
+                );
             }
             else
             {
-                transform.localScale = new Vector3(0.7f, transform.localScale.y, transform.localScale.z);
+                transform.localScale = new Vector3(
+                    0.7f,
+                    transform.localScale.y,
+                    transform.localScale.z
+                );
             }
         }
 
@@ -37,14 +46,20 @@ public class ArrowController : MonoBehaviour
         {
             if (transform.localScale.x > 0.1f)
             {
-                transform.localScale = new Vector3(transform.localScale.x - (1 * Time.deltaTime), transform.localScale.y,
-                    transform.localScale.z);
+                transform.localScale = new Vector3(
+                    transform.localScale.x - (1 * Time.deltaTime),
+                    transform.localScale.y,
+                    transform.localScale.z
+                );
             }
             else
             {
-                transform.localScale = new Vector3(0.1f, transform.localScale.y, transform.localScale.z);
+                transform.localScale = new Vector3(
+                    0.1f,
+                    transform.localScale.y,
+                    transform.localScale.z
+                );
             }
-
         }
     }
 }
